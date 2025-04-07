3fd4154cb46f7de746bddc0a02e19a647b81c6c9
Alfred056 <alfredovilca374@gmail.com>
2025-04-06 22:01:07 -0500

    cambiar nombre

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Millonay
+# millonary
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.16.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'millonay'`, () => {
+  it(`should have as title 'millonary'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('millonay');
+    expect(app.title).toEqual('millonary');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('millonay app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('millonary app is running!');
   });
 });
